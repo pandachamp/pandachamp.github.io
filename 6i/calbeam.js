22158dac5fe4bf83e7a821113730c70eb41f4450
Tiger Mingz <pandachamp@me.com>
2014-10-23 23:42:49 +0700

    change 8

--- 6i/calbeam.js
+++ 6i/calbeam.js
@@ -1,10 +1,11 @@
 // Beam Impact with 6i BY Byakko ENE
 
 function calbeam(){
-	var dis = Number(document.cal.dis.value);
+	var dis = parseFloat(document.cal.dis.value);
 	var wind = Number(document.cal.wind.value);
 	var angwind = Number(document.cal.angwind.value);
 	var windp = Number(document.cal.windp.value);
+	var ballpp = Number(document.cal.ballp.value);
 	
 	//Greencell
 	var greencell = 0.006*wind*Math.sin((angwind*3.14)/180)*dis;
@@ -25,7 +26,13 @@ function calbeam(){
 			ppower = (dis-3)+(wind/3);
 		}
 	}
+	if (ballpp==1){
+		ppower = ppower;
+	}
+	else if(ballpp==2){
 
+		ppower +=1 ;
+	}
 	document.cal.powerp.value = ppower;
 
 	var power = (140*ppower)/100;
